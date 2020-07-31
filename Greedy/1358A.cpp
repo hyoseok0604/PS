8a2b6100484643ca4aabb9e8ee965028ed3b5c06
jhsapps <janghs0604@gmail.com>
2020-07-31 10:21:23 +0900

    Park Lighting / 그리디, 수학

---
+++ Greedy/1358A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long
+#define TC int TC;cin>>TC;while(TC--)
+#define fastIO() ios_base::sync_with_stdio(0),cin.tie(0)
+#define uMin(a, b) a = min(a, b);
+#define uMax(a, b) a = max(a, b);
+#define mset(a, arr) memset(arr, a, sizeof(arr))
+
+int main(){
+    fastIO();
+
+    TC{
+        int n, m;
+        cin >> n >> m;
+
+        cout << (n * m + 1) / 2 << '\n';
+    }
+}
